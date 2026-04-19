7329f1f51915db616ab94c4eb308a118ac10bce0
Bhavesh Shahani <shahanibhavesh40@gmail.com>
2026-04-19 17:22:34 +0530

    update supabase credentials

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 // Replace these with your Supabase project URL and anon key
-const SUPABASE_URL  = "https://chwwlgcipsqogjvupqwd.supabase.co";
-const SUPABASE_ANON = "sb_publishable__6THYwAkXZ83FRtX4ZzUTQ_kPqKNbyv";
+const SUPABASE_URL  = "https://ekughxkikjzkimadyyuk.supabase.co";
+const SUPABASE_ANON = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVrdWdoeGtpa2p6a2ltYWR5eXVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY1ODkxNzQsImV4cCI6MjA5MjE2NTE3NH0.RMROZ2GAcDC6yxY8YjLW3RmyUk2c5G6HnzQry4qA2xs";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON);
